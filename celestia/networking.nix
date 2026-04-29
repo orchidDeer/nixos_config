a2e5cbd89f2474e6072e5bec0bcffc5f0f82a86a
orchidDeer <github@orchiddeer.de>
2026-04-29 17:31:15 +0200

    fix networking

--- celestia/networking.nix
+++ celestia/networking.nix
@@ -4,6 +4,25 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  networking.nameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
+
+  # This forces your nameservers to the TOP of the list
+  # and ignores the ones provided by the router DHCP
+  networking.resolvconf.extraOptions = [
+    "timeout:1"
+    "attempts:1"
+  ];
+  networking.networkmanager.insertNameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
+
+  # Disable the speedport search domain to stop unnecessary lookups
+  networking.search = [ ];
+
   networking.networkmanager.ensureProfiles.profiles = {
     "Direct-Server-Link" = {
       connection = {
@@ -16,10 +35,9 @@
         mac-address = "9c:bf:0d:00:7e:63";
       };
       ipv4 = {
-        method = "manual";
-        address1 = "192.168.10.150/24";
+        method = "auto";
         routes = "192.168.2.111/24,192.168.10.1";
-        never-default = "true";
+        never-default = true;
       };
       ipv6.method = "disabled";
     };
