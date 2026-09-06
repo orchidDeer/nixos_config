63c36e4baac08cc2e5613c09c9f928127f6e914e
orchidDeer <github@orchiddeer.de>
2026-09-06 13:05:09 +0200

    add new syntax zed

--- celestia/home-manager/zeditor.nix
+++ celestia/home-manager/zeditor.nix
@@ -52,14 +52,18 @@
       theme_overrides = {
         "Everforest Blurred" = {
           "syntax" = {
-            "keyword" = {"color" = "#FFD0A6"; "font_style" = "italic"; }; #orange
-            "string" = {"color" = "#CBFFB0"; }; #green
-            "variable" = {"color" = "#FCB8CF"; }; #pink
-            "function" ={"color" = "#BDFCFF"; }; #lightblue
-            "number" = {"color" = "#BDCAFF"; }; #purple
-            "link-text" = {"color" = "#4F58D1"; }; #blue
-            "tag" = {"color" = "#F2D9FF"; }; #lilac
-            "punctuation.bracket" = {"color" = "#E9D2F7"; };
+            "comment" = {
+              "color" = "#C3DECAFF";
+            };
+            "variable" = {
+              "color" = "#FFEBFBFF";
+            };
+            "label" = {
+              "color" = "#7A515100";
+            };
+            "tag" = {
+              "color" = "#C9E5FFFF";
+            };
           };
         };
       };
